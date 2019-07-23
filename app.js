573ab928ba95ca234867f7bffefa04feb2d023db
nexseedcamille <ykamimori@nexseed.net>
2019-07-23 15:08:45 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let score = 60;
+
+if (score >= 80) {
+  console.log(`${score}点、合格`)
+} else if (score >= 50) {
+  console.log(`${score}点、惜しい`);
+} else if (score >= 30) {
+  console.log(`${score}点、残念`);
+} else {
+  console.log(`${score}点、また来年`);
+}
