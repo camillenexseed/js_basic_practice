d48348ae1133c3341a4f6f6cb19b750097a377c2
nexseedcamille <ykamimori@nexseed.net>
2019-07-23 15:14:36 +0900

    forとifを組み合わせた練習
    3の倍数で処理を分岐

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ if (score >= 80) {
 } else {
   console.log(`${score}点、また来年`);
 }
+
+for(let i = 1; i <= 30; i++) {
+  // i % 3 == 0
+  // 3で割った時の余りが０になる時
+  if(i % 3 == 0){
+    console.log('あほ');
+  }else{
+    console.log(i);
+  }
+}
